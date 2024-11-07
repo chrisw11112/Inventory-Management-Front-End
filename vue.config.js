2c05916ce130eaadce2a4bca15b81293be7e5406
Chris Webster <christopher.webs@gmail.com>
2024-11-07 15:41:01 -0600

    Fixing nginx configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     configureWebpack: {
       devtool: 'source-map'
+    },
+    devServer: {
+      port: 8080, 
+      host: '0.0.0.0',
+      public: '0.0.0.0:8080'
     }
   }
